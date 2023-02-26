6041b0537ab3df043db7cdfef8e6d647fdacebfe
Alexander Berryhill <amsb99@gmail.com>
2023-02-25 17:58:55 -0700

    expo camera working!

--- object-detection/pages/FileViewer.js
+++ object-detection/pages/FileViewer.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import DocumentPicker from 'react-native-document-picker'
-
-const res = await DocumentPicker.pick({
-    type: [DocumentPicker.types.allFiles],
-});
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+// import DocumentPicker from 'react-native-document-picker'
 
 
 function FileViewer(){
-    singleFile = res;
-
-    let uri = singleFile.uri;
-    if (Platform.OS === 'ios') {
-        uri = res.uri.replace('file://', '');
-    }
-    FileViewer.open(uri)
-    .then(() => {
-        //Can do anything you want after opening the file successfully
-        console.log('Success');
-    })
-    .catch(_err => {
-        //Handle failure here
-        console.log(_err);
-    });
-
     return(
-        <div></div>
+        <View></View>
     )
 }
 
